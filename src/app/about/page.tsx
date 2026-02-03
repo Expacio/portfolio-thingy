3dd42654b5bbe916f66b60ebad6a3df03cd5958a
Expacio <akshay.mishra2228@gmail.com>
2026-02-04 01:56:58 +0530

    ok

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,7 +24,7 @@ export default function Projects() {
 
     
       </div>
-      <p className="text-xl text-zinc leading-relaxed">
+      <p className="text-3xl justify-center text-zinc leading-relaxed">
         Socials
       </p>
       <div className="flex gap-8 justify-center items-center py-10">
